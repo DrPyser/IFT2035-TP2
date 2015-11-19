24e6116640c42fd77a7a1313b07cf7f00686d42b
drpyser <schok53@gmail.com>
2015-11-19 16:13:10 -0500

    Pas mal fini. Je vais essayer de voir si c'est utile d'utiliser des monads(state et continuation) pour maximiser l'abstraction.

--- tp2.scm
+++ tp2.scm
@@ -1,4 +1,4 @@
-#! /usr/bin/env gsi -:dR
+#! /usr/bin/gsi -:dR
 
 ;;; Fichier : tp2.scm
 
@@ -22,6 +22,43 @@
 ;;; fonctions ne doivent pas faire d'affichage car c'est la fonction
 ;;; "repl" qui se charge de cela.
 
+;(load "/usr/lib64/gambit-c/syntax-case.scm")
+
+;;State monad
+(define st-return
+  (lambda (x)
+    (lambda (s)
+      (cons x s))))
+
+(define get
+  (lambda (s)
+    (cons s s)))
+
+(define (put ss)
+  (lambda (s)
+    (cons '() ss)))
+
+(define (run st s)
+  (st s))
+
+(define (st-bind st f)
+  (lambda (s)
+    (let* ((a (run st s))
+	   (x (car a))
+	   (stt (cdr a)))
+      (run (f x) stt))))
+
+(define (modify f)
+  (lambda (s)
+    (cons '() (f s))))
+
+(define (gets f)
+  (lambda (s)
+    (cons (f s) s)))
+;;-------------------------------------------------------------------------
+
+;;;Fonctions utilitaires
+
 (define foldl
   (lambda (f base lst)
     (if (null? lst)
@@ -44,12 +81,12 @@
       [(#\9) 9])))
 
 (define del-assoc
-  (lambda (key assoc)
-    (if (null? assoc)
-	assoc
-	(if (eqv? (caar assoc) key)
-	    (cdr assoc)
-	    (cons (car assoc) (del-assoc (cdr assoc)))))))
+  (lambda (key lst)
+    (if (null? lst)
+	lst
+	(if (eqv? (caar lst) key)
+	    (cdr lst)
+	    (cons (car lst) (del-assoc key (cdr lst)))))))
 
 (define ensure-assoc
   (lambda (lst key value)
@@ -61,13 +98,13 @@
 ;;ex: (span odd? '(1 3 5 6 7 8 9)) => ((1 3 5) (6 7 8 9))
 (define span
   (lambda (p lst)
-    (define (helper accum rest)
+    (let helper ((accum '())
+		 (rest lst))
       (if (null? rest)
 	  (list (reverse accum) rest)
 	  (if (p (car rest))
 	      (helper (cons (car rest) accum) (cdr rest))
-	      (list (reverse accum) rest))))
-    (helper '() lst)))	
+	      (list (reverse accum) rest))))))
 
 ;;Convertit une liste de charactère représentant des chiffres en le nombre correspondant à la lecture de gauche à droite
 ;;ex:(chars->number '(#\1 #\2 #\3)) => 123
@@ -86,13 +123,6 @@
 	  (let ((tmp (span (lambda (x) (not (p x))) lst)))
 	    (cons (car tmp) (split p (cadr tmp)))))))
 
-(define space?
-  (lambda (char)
-    (char=? char #\ )))
-
-(define traiter-jeton)
-
-
 (define (take n lst)
   (if (null? lst)
       '()
@@ -107,6 +137,14 @@
 	  lst
 	  (drop (- n 1) (cdr lst)))))
 
+(define take-while
+  (lambda (p lst)
+    (let helper ((lst lst)
+		 (cont (lambda (x) x)))
+      (if (p (car lst))
+	  (helper (cdr lst) (lambda (x) (cons (car lst) x)))
+	  (cont '())))))
+
 (define (split-at n lst)
   (let helper ((taken '())
 	       (rest lst)
@@ -114,56 +152,98 @@
     (if (or (<= n 0) (null? rest))
 	(list (reverse taken) rest)
 	(helper (cons (car rest) taken) (cdr rest) (- n 1)))))
-
-(define compute
-  (lambda (dict stack nargs operator)
-    (if (>= (length stack) nargs)
-	(let ((tmp (split-at nargs stack)))
-	  (list #f (cons (apply operator (car tmp)) (cadr tmp)) dict))
-	(list "*** Erreur: Nombre d'argument insuffisant." stack dict))))
-
-(define traiter-liste
-  (lambda (acc token)
-    (let* ((error (car acc))
-	   (stk (cadr acc)) 
-	   (dict (caddr acc))
-	   (top (if (null? stk) #f (car stk))))
-      (if error
-	  acc
-	  (cond ((number? token) (list #f (cons token stk) dict))
-		((char? token) (let ((value (assoc token dict)))
-				 (if value
-				     (list nil (cons value stk) dict)
-				     (list "*** Erreur: variable non-définie ***" stk dict))))
-		((symbol? token) (case token
-				   [(+) (compute stk + 2)]
-				   [(-) (compute stk - 2)]
-				   [(*) (compute stk * 2)]))		  
-		((string? token) (list token stk dict))
-		((list? token) (case (car token)
-				 [(=) (if (null? stk)
-					  (list "*** Erreur: argument manquant pour l'affectation ***" stk dict)
-					  (list #f stk (ensure-assoc dict (cadr token) (car stk))))]))
-		(t (list "what the fuck is that?" stk dict)))))))
 				  
 (define number->list
   (lambda (number)
     (if (number? number)
 	(string->list (number->string number))
 	'())))
-				    
+
+(define every
+  (lambda (p lst)
+    (or (null? lst)
+	(if (p (car lst))
+	    (every p (cdr lst))
+	    #f))))
+
+(define operator?
+  (lambda (char)
+    (member char (list #\+ #\- #\* #\/))))
+
+;;Une macro pour destructurer une liste
+
+;(define-syntax let+
+ ; (syntax-rules ()
+  ;  [(_ pattern lst f ...) (apply (lambda pattern f ...) lst)]))
+
+(define operator->procedure
+  (lambda (char)
+    (case char
+      ((#\+) (list + 2))
+      ((#\*) (list * 2))
+      ((#\-) (list - 2))
+      ((#\/) (list / 2)))))      
+
+;;----------------------------------------------------------------------------
+;;Fonction pour traiter un jeton, en retournant à la continuation une pile et un dictionnaire potentiellement modifié
+;;(et potentiellement un message d'erreur)
+(define traiter-jeton
+  (lambda (jeton pile dict cont)
+    (cond ((char-numeric? (car jeton)) (if (every char-numeric? jeton)
+					   (cont #f (cons (chars->number jeton) pile) dict)
+					   (cont (string-append "*** Erreur: jeton invalide: " (list->string jeton) " ***\n")
+						 pile dict)))
+	  ((char-alphabetic? (car jeton)) (if (= (length jeton) 1)
+					      (let ((binding (assoc (car jeton) dict)))
+						(if binding
+						    (cont #f (cons (cdr binding) pile) dict)
+						    (cont (string-append "*** Erreur: variable non-définie: " (string (car jeton)) " ***\n")
+							  pile
+							  dict)))
+						(cont (string-append "*** Erreur: jeton invalide: " (list->string jeton) " ***\n")
+						      pile dict)))
+	  ((char=? (car jeton) #\=) (if (= (length jeton) 2)
+					(if (char-alphabetic? (cadr jeton))
+					    (if (> (length pile) 0)
+						(cont #f pile (ensure-assoc dict (cadr jeton) (car pile)))
+						(cont "*** Erreur: argument manquant pour l'opération d'assignation ***\n"
+						      pile dict))
+					    (cont "*** Erreur: jeton invalide - une variable doit être un charactère alphabetic ***\n"
+						  pile dict))
+					(cont (string-append "*** Erreur: jeton invalide: " (list->string jeton) " ***\n")
+					      pile dict)))
+	  ((operator? (car jeton)) (apply (lambda (proc narg)
+					    (if (>= (length pile) narg)
+						(cont #f (cons (apply proc (take narg pile)) (drop narg pile)) dict)
+						(cont (string-append "*** Erreur: nombre d'arguments insuffisant pour l'opérateur "
+								     (string (car jeton)) " ***\n") pile dict)))
+						      (operator->procedure (car jeton))))
+	  (else (cont "What the fuck is that?\n" pile dict)))))
+						   
+
+;;Traite l'ensemble des jeton. La continuation peut retourner à n'importe quel étape du traitement dans le cas d'une erreur
+(define traiter-jetons
+  (lambda (jetons pile dict cont)
+    (if (null? jetons)
+	(cont #f pile dict)
+	(traiter-jeton (car jetons) pile dict 
+			(lambda (erreur pile dict)
+			  (if erreur
+			      (cons (string->list erreur) dict)
+			      (traiter-jetons (cdr jetons) pile dict cont)))))))
+
 (define traiter
   (lambda (expr dict)
     (if (null? expr)
-	(list '() dict)
-	(let* ((results (foldl traiter-liste (list #f '() dict) (map traiter-jeton (split space? expr))))
-	       (erreur (car results))
-	       (stk (cadr results))
-	       (dict (caddr results)))
-	  (cond (erreur (list (string->list erreur) dict))
-		((null? stk) (list '() dict))
-		((> (length stk) 1) (cons (string->list "*** Erreur: valeur(s) inutilisée(s) ***") dict))
-		(else (list (number->list (car stk)) dict)))))))
+	(cons '() dict)
+	(let ((tokens (split char-whitespace? expr)))
+;	  (display tokens)
+	  (traiter-jetons tokens '() dict (lambda (erreur pile dict)
+					(if erreur
+					    (cons (string->list erreur) dict)
+					    (cond ((null? pile) (cons '() dict))
+						  ((> (length pile) 1) (cons (string->list "*** Erreur: valeur(s) inutilisée(s) ***") dict))
+						  (else (cons (number->list (car pile)) dict))))))))))
 
 ;;;----------------------------------------------------------------------------
 
@@ -179,6 +259,7 @@
       (if (string? ligne)
           (let ((r (traiter-ligne ligne dict)))
             (for-each write-char (car r))
+;	    (display (cadr r))
             (repl (cdr r)))))))
 
 (define traiter-ligne
